74d69ca24fd67eb32aa0ed700f6947018acbab36
Flow-Pie <startabase@gmail.com>
2025-11-22 10:34:16 +0300

    fix(response-interceptor): add return type annotation
    Signed-off-by: Flow-Pie <startabase@gmail.com>
    Co-authored-by: pixell-droid <pixell-droid@users.noreply.github.com>

--- apps/api/src/common/interceptors/response.interceptor.ts
+++ apps/api/src/common/interceptors/response.interceptor.ts
@@ -4,11 +4,15 @@ import {
   ExecutionContext,
   CallHandler,
 } from '@nestjs/common';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ResponseInterceptor implements NestInterceptor {
-  intercept(_context: ExecutionContext, next: CallHandler) {
+  intercept(
+    _context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<{ success: boolean; data: unknown }> {
     return next.handle().pipe(
       map((data) => ({
         success: true,
