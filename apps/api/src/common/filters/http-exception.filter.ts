c21a61e1457e06046d0f5c919699275a20af1a31
Flow-Pie <startabase@gmail.com>
2025-11-22 10:33:51 +0300

    fix(http-exception-filter): add strict type safety
    Signed-off-by: Flow-Pie <startabase@gmail.com>
    Co-authored-by: pixell-droid <pixell-droid@users.noreply.github.com>

--- apps/api/src/common/filters/http-exception.filter.ts
+++ apps/api/src/common/filters/http-exception.filter.ts
@@ -5,18 +5,27 @@ import {
   HttpException,
 } from '@nestjs/common';
 
+interface HttpExceptionResponse {
+  message?: string;
+}
+
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
-  catch(exception: any, host: ArgumentsHost) {
+  catch(exception: unknown, host: ArgumentsHost): void {
     const ctx = host.switchToHttp();
     const res = ctx.getResponse();
     const status =
       exception instanceof HttpException ? exception.getStatus() : 500;
+    
+    const message =
+      exception instanceof HttpException
+        ? (exception.getResponse() as HttpExceptionResponse).message || exception.message
+        : 'Internal server error';
 
     res.status(status).json({
       success: false,
       statusCode: status,
-      message: exception.message || 'Internal server error',
+      message,
     });
   }
 }
